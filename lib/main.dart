904467ae97e84d5c53915bbdf0efc69081d3ea8d
Vichiuri <chiuri.vic@gmail.com>
2022-11-11 15:54:45 +0300

    delivery

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'home_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bottom NavBar Demo',
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 111, 92, 194),
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
